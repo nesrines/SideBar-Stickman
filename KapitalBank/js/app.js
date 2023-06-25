a1b21c988a1c8df69a7d94ff92851a514613555d
Nasrin Asadli <nasrinasadli@Atillas-MacBook-Air.local>
2023-06-25 12:57:10 +0400

    third

--- KapitalBank/js/app.js
+++ KapitalBank/js/app.js
@@ -1,3 +1,14 @@
-document.querySelector("ms-3").addEventListener("click", () => {
+let btn = document.querySelector(".icons .ms-3");
+let icon = document.querySelector(".icons .ms-3 .fas")
+btn.addEventListener("click", () => {
+    if (icon.className === "fas fa-xmark") {
+        btn.style.fontSize = "17px";
+        icon.classList.replace("fa-xmark", "fa-bars");
+    }
+    else {
+        btn.style.fontSize = "22px";
+        icon.classList.replace("fa-bars", "fa-xmark");
+    }
+    btn.classList.toggle("active");
     document.querySelector("#SideBar").classList.toggle("d-none");
-})
+});
