4ed57c8f7c5f01662a883805da6ec75140fa4200
Nasrin F. Asadli <nasrinfa@code.local>
2023-06-25 15:07:23 +0400

    fourth

--- KapitalBank/js/app.js
+++ KapitalBank/js/app.js
@@ -1,14 +1,6 @@
-let btn = document.querySelector(".icons .ms-3");
-let icon = document.querySelector(".icons .ms-3 .fas")
-btn.addEventListener("click", () => {
-    if (icon.className === "fas fa-xmark") {
-        btn.style.fontSize = "17px";
-        icon.classList.replace("fa-xmark", "fa-bars");
-    }
-    else {
-        btn.style.fontSize = "22px";
-        icon.classList.replace("fa-bars", "fa-xmark");
-    }
-    btn.classList.toggle("active");
-    document.querySelector("#SideBar").classList.toggle("d-none");
+document.querySelector("#Open").addEventListener("click", () => {
+    document.querySelector(".sidebar").style.width = "100%";
+})
+document.querySelector("#Close").addEventListener("click", () => {
+    document.querySelector(".sidebar").style.width = "0%";
 });
